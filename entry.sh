c615ac4ae3b178a2248f2e7255a5d6cfdb0e8670
Mel Lowiz <mellowiz@hotmail.com>
2019-10-07 15:06:04 +0200

    Extended wait time in entry.sh

--- entry.sh
+++ entry.sh
@@ -6,12 +6,12 @@ PORT=5555						# Destination port
 STRING1=$(/bin/hostname)				# Just some dummy text to identify ourselves, hostname
 STRING2=$(ip a | tail -n2 | awk '/inet/ {print $2}')	# ... IP address
 DATE=$(date +"%Y/%m/%d %H:%M")				# Current date
-MINW=10							# Minimum wait time
-MAXW=30							# Maximum wait time
+MINW=60							# Minimum wait time [s]
+MAXW=300						# Maximum wait time [s]
 
 # Main loop, the script will stay on forever
 while true
 do
     echo $STRING1" "$STRING2" "$DATE | /usr/bin/nc $TARGET $PORT				# Pipe the text into Netcat
-    sleep $(awk -v MINW=5 -v MAXW=10 'BEGIN{srand(); print int(MINW+rand()*(MAXW-MINW+1))}')	# Sleep MINW < T < MAXW
+    sleep $(awk -v MIN=$MINW -v MAX=$MAXW 'BEGIN{srand(); print int(MIN+rand()*(MAX-MIN+1))}')	# Sleep MINW < T < MAXW
 done
